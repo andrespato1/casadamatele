805ec121a0edacdc93701c700cb97eadccec9c19
jorge <andrespato1@gmail.com>
2016-11-20 00:10:28 -0600

    mas botones y otros

--- PrograFinalBD/PrograFinalBD/MenuFuncionario.Designer.cs
+++ PrograFinalBD/PrograFinalBD/MenuFuncionario.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.BtnAtrasVtnaMenuFuncionarios = new System.Windows.Forms.Button();
+            this.BtnAgregarFuncionarioVtnMenuFuncionario = new System.Windows.Forms.Button();
+            this.BtnAgregarPacienteVtnaMenuPaciente = new System.Windows.Forms.Button();
+            this.BtnSacarCitasVtnaMenuPaciente = new System.Windows.Forms.Button();
+            this.BtnCancelarCitasVtnaMenuPaciente = new System.Windows.Forms.Button();
+            this.BtnAtenderPacienteVtnaMenuPaciente = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -37,7 +42,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(115, 26);
             this.label1.TabIndex = 0;
@@ -55,12 +60,77 @@
             this.BtnAtrasVtnaMenuFuncionarios.UseVisualStyleBackColor = false;
             this.BtnAtrasVtnaMenuFuncionarios.Click += new System.EventHandler(this.BtnAtrasVtnaMenuFuncionarios_Click);
             // 
+            // BtnAgregarFuncionarioVtnMenuFuncionario
+            // 
+            this.BtnAgregarFuncionarioVtnMenuFuncionario.BackColor = System.Drawing.Color.Azure;
+            this.BtnAgregarFuncionarioVtnMenuFuncionario.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAgregarFuncionarioVtnMenuFuncionario.ForeColor = System.Drawing.Color.Indigo;
+            this.BtnAgregarFuncionarioVtnMenuFuncionario.Location = new System.Drawing.Point(17, 38);
+            this.BtnAgregarFuncionarioVtnMenuFuncionario.Name = "BtnAgregarFuncionarioVtnMenuFuncionario";
+            this.BtnAgregarFuncionarioVtnMenuFuncionario.Size = new System.Drawing.Size(93, 45);
+            this.BtnAgregarFuncionarioVtnMenuFuncionario.TabIndex = 2;
+            this.BtnAgregarFuncionarioVtnMenuFuncionario.Text = "Agregar Funcionario";
+            this.BtnAgregarFuncionarioVtnMenuFuncionario.UseVisualStyleBackColor = false;
+            // 
+            // BtnAgregarPacienteVtnaMenuPaciente
+            // 
+            this.BtnAgregarPacienteVtnaMenuPaciente.BackColor = System.Drawing.Color.Azure;
+            this.BtnAgregarPacienteVtnaMenuPaciente.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAgregarPacienteVtnaMenuPaciente.ForeColor = System.Drawing.Color.Indigo;
+            this.BtnAgregarPacienteVtnaMenuPaciente.Location = new System.Drawing.Point(116, 38);
+            this.BtnAgregarPacienteVtnaMenuPaciente.Name = "BtnAgregarPacienteVtnaMenuPaciente";
+            this.BtnAgregarPacienteVtnaMenuPaciente.Size = new System.Drawing.Size(93, 45);
+            this.BtnAgregarPacienteVtnaMenuPaciente.TabIndex = 3;
+            this.BtnAgregarPacienteVtnaMenuPaciente.Text = "Agregar Pacientes";
+            this.BtnAgregarPacienteVtnaMenuPaciente.UseVisualStyleBackColor = false;
+            // 
+            // BtnSacarCitasVtnaMenuPaciente
+            // 
+            this.BtnSacarCitasVtnaMenuPaciente.BackColor = System.Drawing.Color.Azure;
+            this.BtnSacarCitasVtnaMenuPaciente.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSacarCitasVtnaMenuPaciente.ForeColor = System.Drawing.Color.Indigo;
+            this.BtnSacarCitasVtnaMenuPaciente.Location = new System.Drawing.Point(215, 38);
+            this.BtnSacarCitasVtnaMenuPaciente.Name = "BtnSacarCitasVtnaMenuPaciente";
+            this.BtnSacarCitasVtnaMenuPaciente.Size = new System.Drawing.Size(93, 45);
+            this.BtnSacarCitasVtnaMenuPaciente.TabIndex = 4;
+            this.BtnSacarCitasVtnaMenuPaciente.Text = "Asignar Citas";
+            this.BtnSacarCitasVtnaMenuPaciente.UseVisualStyleBackColor = false;
+            // 
+            // BtnCancelarCitasVtnaMenuPaciente
+            // 
+            this.BtnCancelarCitasVtnaMenuPaciente.BackColor = System.Drawing.Color.Azure;
+            this.BtnCancelarCitasVtnaMenuPaciente.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCancelarCitasVtnaMenuPaciente.ForeColor = System.Drawing.Color.Indigo;
+            this.BtnCancelarCitasVtnaMenuPaciente.Location = new System.Drawing.Point(314, 38);
+            this.BtnCancelarCitasVtnaMenuPaciente.Name = "BtnCancelarCitasVtnaMenuPaciente";
+            this.BtnCancelarCitasVtnaMenuPaciente.Size = new System.Drawing.Size(93, 45);
+            this.BtnCancelarCitasVtnaMenuPaciente.TabIndex = 5;
+            this.BtnCancelarCitasVtnaMenuPaciente.Text = "Cancelar Citas";
+            this.BtnCancelarCitasVtnaMenuPaciente.UseVisualStyleBackColor = false;
+            // 
+            // BtnAtenderPacienteVtnaMenuPaciente
+            // 
+            this.BtnAtenderPacienteVtnaMenuPaciente.BackColor = System.Drawing.Color.Azure;
+            this.BtnAtenderPacienteVtnaMenuPaciente.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAtenderPacienteVtnaMenuPaciente.ForeColor = System.Drawing.Color.Indigo;
+            this.BtnAtenderPacienteVtnaMenuPaciente.Location = new System.Drawing.Point(413, 38);
+            this.BtnAtenderPacienteVtnaMenuPaciente.Name = "BtnAtenderPacienteVtnaMenuPaciente";
+            this.BtnAtenderPacienteVtnaMenuPaciente.Size = new System.Drawing.Size(93, 45);
+            this.BtnAtenderPacienteVtnaMenuPaciente.TabIndex = 6;
+            this.BtnAtenderPacienteVtnaMenuPaciente.Text = "Atender Paciente";
+            this.BtnAtenderPacienteVtnaMenuPaciente.UseVisualStyleBackColor = false;
+            // 
             // MenuFuncionario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::PrograFinalBD.Properties.Resources._9290217_M_dico_de_dibujos_animados_que_asisten_a_un_joven_paciente_en_una_sala_de_hospital__Foto_de_archivo1;
             this.ClientSize = new System.Drawing.Size(586, 373);
+            this.Controls.Add(this.BtnAtenderPacienteVtnaMenuPaciente);
+            this.Controls.Add(this.BtnCancelarCitasVtnaMenuPaciente);
+            this.Controls.Add(this.BtnSacarCitasVtnaMenuPaciente);
+            this.Controls.Add(this.BtnAgregarPacienteVtnaMenuPaciente);
+            this.Controls.Add(this.BtnAgregarFuncionarioVtnMenuFuncionario);
             this.Controls.Add(this.BtnAtrasVtnaMenuFuncionarios);
             this.Controls.Add(this.label1);
             this.Name = "MenuFuncionario";
@@ -74,5 +144,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button BtnAtrasVtnaMenuFuncionarios;
+        private System.Windows.Forms.Button BtnAgregarFuncionarioVtnMenuFuncionario;
+        private System.Windows.Forms.Button BtnAgregarPacienteVtnaMenuPaciente;
+        private System.Windows.Forms.Button BtnSacarCitasVtnaMenuPaciente;
+        private System.Windows.Forms.Button BtnCancelarCitasVtnaMenuPaciente;
+        private System.Windows.Forms.Button BtnAtenderPacienteVtnaMenuPaciente;
     }
 }
--- PrograFinalBD/PrograFinalBD/MenuPaciente.Designer.cs
+++ PrograFinalBD/PrograFinalBD/MenuPaciente.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.BtnAtrasVtnaMenuPaciente = new System.Windows.Forms.Button();
+            this.BtnSacarCitaVtnMenuPaciente = new System.Windows.Forms.Button();
+            this.BtnCancelarCitaVtnaMenuPaciente = new System.Windows.Forms.Button();
+            this.BtnDiagnosticosVtnaMenuPaciente = new System.Windows.Forms.Button();
+            this.BtnTratamientoVtnaMenuPaciente = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -37,7 +41,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(120, 26);
             this.label1.TabIndex = 0;
@@ -55,12 +59,64 @@
             this.BtnAtrasVtnaMenuPaciente.UseVisualStyleBackColor = false;
             this.BtnAtrasVtnaMenuPaciente.Click += new System.EventHandler(this.BtnAtrasVtnaMenuPaciente_Click);
             // 
+            // BtnSacarCitaVtnMenuPaciente
+            // 
+            this.BtnSacarCitaVtnMenuPaciente.BackColor = System.Drawing.Color.Turquoise;
+            this.BtnSacarCitaVtnMenuPaciente.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSacarCitaVtnMenuPaciente.ForeColor = System.Drawing.Color.Maroon;
+            this.BtnSacarCitaVtnMenuPaciente.Location = new System.Drawing.Point(11, 36);
+            this.BtnSacarCitaVtnMenuPaciente.Name = "BtnSacarCitaVtnMenuPaciente";
+            this.BtnSacarCitaVtnMenuPaciente.Size = new System.Drawing.Size(75, 47);
+            this.BtnSacarCitaVtnMenuPaciente.TabIndex = 2;
+            this.BtnSacarCitaVtnMenuPaciente.Text = "Sacar Cita";
+            this.BtnSacarCitaVtnMenuPaciente.UseVisualStyleBackColor = false;
+            // 
+            // BtnCancelarCitaVtnaMenuPaciente
+            // 
+            this.BtnCancelarCitaVtnaMenuPaciente.BackColor = System.Drawing.Color.Turquoise;
+            this.BtnCancelarCitaVtnaMenuPaciente.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCancelarCitaVtnaMenuPaciente.ForeColor = System.Drawing.Color.Maroon;
+            this.BtnCancelarCitaVtnaMenuPaciente.Location = new System.Drawing.Point(92, 36);
+            this.BtnCancelarCitaVtnaMenuPaciente.Name = "BtnCancelarCitaVtnaMenuPaciente";
+            this.BtnCancelarCitaVtnaMenuPaciente.Size = new System.Drawing.Size(75, 47);
+            this.BtnCancelarCitaVtnaMenuPaciente.TabIndex = 3;
+            this.BtnCancelarCitaVtnaMenuPaciente.Text = "Cancelar Cita";
+            this.BtnCancelarCitaVtnaMenuPaciente.UseVisualStyleBackColor = false;
+            // 
+            // BtnDiagnosticosVtnaMenuPaciente
+            // 
+            this.BtnDiagnosticosVtnaMenuPaciente.BackColor = System.Drawing.Color.Turquoise;
+            this.BtnDiagnosticosVtnaMenuPaciente.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDiagnosticosVtnaMenuPaciente.ForeColor = System.Drawing.Color.Maroon;
+            this.BtnDiagnosticosVtnaMenuPaciente.Location = new System.Drawing.Point(173, 36);
+            this.BtnDiagnosticosVtnaMenuPaciente.Name = "BtnDiagnosticosVtnaMenuPaciente";
+            this.BtnDiagnosticosVtnaMenuPaciente.Size = new System.Drawing.Size(97, 47);
+            this.BtnDiagnosticosVtnaMenuPaciente.TabIndex = 4;
+            this.BtnDiagnosticosVtnaMenuPaciente.Text = "Diagnosticos";
+            this.BtnDiagnosticosVtnaMenuPaciente.UseVisualStyleBackColor = false;
+            // 
+            // BtnTratamientoVtnaMenuPaciente
+            // 
+            this.BtnTratamientoVtnaMenuPaciente.BackColor = System.Drawing.Color.Turquoise;
+            this.BtnTratamientoVtnaMenuPaciente.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTratamientoVtnaMenuPaciente.ForeColor = System.Drawing.Color.Maroon;
+            this.BtnTratamientoVtnaMenuPaciente.Location = new System.Drawing.Point(276, 36);
+            this.BtnTratamientoVtnaMenuPaciente.Name = "BtnTratamientoVtnaMenuPaciente";
+            this.BtnTratamientoVtnaMenuPaciente.Size = new System.Drawing.Size(97, 47);
+            this.BtnTratamientoVtnaMenuPaciente.TabIndex = 5;
+            this.BtnTratamientoVtnaMenuPaciente.Text = "Tratamientos";
+            this.BtnTratamientoVtnaMenuPaciente.UseVisualStyleBackColor = false;
+            // 
             // MenuPaciente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::PrograFinalBD.Properties.Resources._9290217_M_dico_de_dibujos_animados_que_asisten_a_un_joven_paciente_en_una_sala_de_hospital__Foto_de_archivo1;
             this.ClientSize = new System.Drawing.Size(585, 374);
+            this.Controls.Add(this.BtnTratamientoVtnaMenuPaciente);
+            this.Controls.Add(this.BtnDiagnosticosVtnaMenuPaciente);
+            this.Controls.Add(this.BtnCancelarCitaVtnaMenuPaciente);
+            this.Controls.Add(this.BtnSacarCitaVtnMenuPaciente);
             this.Controls.Add(this.BtnAtrasVtnaMenuPaciente);
             this.Controls.Add(this.label1);
             this.Name = "MenuPaciente";
@@ -75,5 +131,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button BtnAtrasVtnaMenuPaciente;
+        private System.Windows.Forms.Button BtnSacarCitaVtnMenuPaciente;
+        private System.Windows.Forms.Button BtnCancelarCitaVtnaMenuPaciente;
+        private System.Windows.Forms.Button BtnDiagnosticosVtnaMenuPaciente;
+        private System.Windows.Forms.Button BtnTratamientoVtnaMenuPaciente;
     }
 }
