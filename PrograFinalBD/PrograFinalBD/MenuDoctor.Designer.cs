45fbc7fe9b9e049f16509657fb0633440bdb5cfc
jorge andres mora <andrespato1@gmail.com>
2016-11-27 19:40:50 -0600

    cambios

---
+++ PrograFinalBD/PrograFinalBD/MenuDoctor.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace PrograFinalBD
+{
+    partial class MenuDoctor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCancelarCita = new System.Windows.Forms.Button();
+            this.BtnAsignarCita = new System.Windows.Forms.Button();
+            this.BtnCambiarEstadoCita = new System.Windows.Forms.Button();
+            this.BtnAtenderCita = new System.Windows.Forms.Button();
+            this.BtnEjecutarReportes = new System.Windows.Forms.Button();
+            this.BtnAtrasMenuDoctor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnCancelarCita
+            // 
+            this.BtnCancelarCita.BackColor = System.Drawing.Color.Lime;
+            this.BtnCancelarCita.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCancelarCita.Location = new System.Drawing.Point(12, 12);
+            this.BtnCancelarCita.Name = "BtnCancelarCita";
+            this.BtnCancelarCita.Size = new System.Drawing.Size(132, 49);
+            this.BtnCancelarCita.TabIndex = 0;
+            this.BtnCancelarCita.Text = "Cancelar Cita";
+            this.BtnCancelarCita.UseVisualStyleBackColor = false;
+            // 
+            // BtnAsignarCita
+            // 
+            this.BtnAsignarCita.BackColor = System.Drawing.Color.Lime;
+            this.BtnAsignarCita.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAsignarCita.Location = new System.Drawing.Point(12, 67);
+            this.BtnAsignarCita.Name = "BtnAsignarCita";
+            this.BtnAsignarCita.Size = new System.Drawing.Size(132, 49);
+            this.BtnAsignarCita.TabIndex = 1;
+            this.BtnAsignarCita.Text = "Asignar Cita";
+            this.BtnAsignarCita.UseVisualStyleBackColor = false;
+            // 
+            // BtnCambiarEstadoCita
+            // 
+            this.BtnCambiarEstadoCita.BackColor = System.Drawing.Color.Lime;
+            this.BtnCambiarEstadoCita.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCambiarEstadoCita.Location = new System.Drawing.Point(12, 122);
+            this.BtnCambiarEstadoCita.Name = "BtnCambiarEstadoCita";
+            this.BtnCambiarEstadoCita.Size = new System.Drawing.Size(132, 49);
+            this.BtnCambiarEstadoCita.TabIndex = 2;
+            this.BtnCambiarEstadoCita.Text = "Cambiar Estado De Cita";
+            this.BtnCambiarEstadoCita.UseVisualStyleBackColor = false;
+            // 
+            // BtnAtenderCita
+            // 
+            this.BtnAtenderCita.BackColor = System.Drawing.Color.Lime;
+            this.BtnAtenderCita.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAtenderCita.Location = new System.Drawing.Point(12, 177);
+            this.BtnAtenderCita.Name = "BtnAtenderCita";
+            this.BtnAtenderCita.Size = new System.Drawing.Size(132, 49);
+            this.BtnAtenderCita.TabIndex = 3;
+            this.BtnAtenderCita.Text = "Atender Cita";
+            this.BtnAtenderCita.UseVisualStyleBackColor = false;
+            // 
+            // BtnEjecutarReportes
+            // 
+            this.BtnEjecutarReportes.BackColor = System.Drawing.Color.Lime;
+            this.BtnEjecutarReportes.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEjecutarReportes.Location = new System.Drawing.Point(12, 232);
+            this.BtnEjecutarReportes.Name = "BtnEjecutarReportes";
+            this.BtnEjecutarReportes.Size = new System.Drawing.Size(132, 49);
+            this.BtnEjecutarReportes.TabIndex = 4;
+            this.BtnEjecutarReportes.Text = "Ejecutar Reportes";
+            this.BtnEjecutarReportes.UseVisualStyleBackColor = false;
+            // 
+            // BtnAtrasMenuDoctor
+            // 
+            this.BtnAtrasMenuDoctor.BackColor = System.Drawing.Color.Red;
+            this.BtnAtrasMenuDoctor.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAtrasMenuDoctor.Location = new System.Drawing.Point(473, 482);
+            this.BtnAtrasMenuDoctor.Name = "BtnAtrasMenuDoctor";
+            this.BtnAtrasMenuDoctor.Size = new System.Drawing.Size(99, 67);
+            this.BtnAtrasMenuDoctor.TabIndex = 5;
+            this.BtnAtrasMenuDoctor.Text = "ATRAS";
+            this.BtnAtrasMenuDoctor.UseVisualStyleBackColor = false;
+            // 
+            // MenuDoctor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PrograFinalBD.Properties.Resources._9290217_M_dico_de_dibujos_animados_que_asisten_a_un_joven_paciente_en_una_sala_de_hospital__Foto_de_archivo1;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.BtnAtrasMenuDoctor);
+            this.Controls.Add(this.BtnEjecutarReportes);
+            this.Controls.Add(this.BtnAtenderCita);
+            this.Controls.Add(this.BtnCambiarEstadoCita);
+            this.Controls.Add(this.BtnAsignarCita);
+            this.Controls.Add(this.BtnCancelarCita);
+            this.Name = "MenuDoctor";
+            this.Text = "MenuDoctor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCancelarCita;
+        private System.Windows.Forms.Button BtnAsignarCita;
+        private System.Windows.Forms.Button BtnCambiarEstadoCita;
+        private System.Windows.Forms.Button BtnAtenderCita;
+        private System.Windows.Forms.Button BtnEjecutarReportes;
+        private System.Windows.Forms.Button BtnAtrasMenuDoctor;
+    }
+}
