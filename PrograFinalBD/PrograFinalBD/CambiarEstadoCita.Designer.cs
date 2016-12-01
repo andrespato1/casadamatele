aae6366c5f8566b958f66285731f6c77a6a36937
jorge andres mora <andrespato1@gmail.com>
2016-12-01 02:00:28 -0600

    mas cambios

--- PrograFinalBD/PrograFinalBD/CambiarEstadoCita.Designer.cs
+++ PrograFinalBD/PrograFinalBD/CambiarEstadoCita.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtNumCita = new System.Windows.Forms.TextBox();
+            this.txtEstadoCita = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnAtras = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,10 +53,56 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 105);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 130);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(561, 445);
+            this.dataGridView1.Size = new System.Drawing.Size(561, 420);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // txtNumCita
+            // 
+            this.txtNumCita.Location = new System.Drawing.Point(103, 43);
+            this.txtNumCita.Name = "txtNumCita";
+            this.txtNumCita.Size = new System.Drawing.Size(100, 20);
+            this.txtNumCita.TabIndex = 2;
+            // 
+            // txtEstadoCita
+            // 
+            this.txtEstadoCita.Location = new System.Drawing.Point(103, 104);
+            this.txtEstadoCita.Name = "txtEstadoCita";
+            this.txtEstadoCita.Size = new System.Drawing.Size(100, 20);
+            this.txtEstadoCita.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingrese el numero de cita";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(100, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ingrese el nuevo estado de la  cita";
+            // 
+            // BtnAtras
+            // 
+            this.BtnAtras.BackColor = System.Drawing.Color.Red;
+            this.BtnAtras.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAtras.Location = new System.Drawing.Point(486, 12);
+            this.BtnAtras.Name = "BtnAtras";
+            this.BtnAtras.Size = new System.Drawing.Size(87, 51);
+            this.BtnAtras.TabIndex = 6;
+            this.BtnAtras.Text = "ATRAS";
+            this.BtnAtras.UseVisualStyleBackColor = false;
+            this.BtnAtras.Click += new System.EventHandler(this.BtnAtras_Click);
             // 
             // CambiarEstadoCita
             // 
@@ -59,6 +110,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::PrograFinalBD.Properties.Resources._9290217_M_dico_de_dibujos_animados_que_asisten_a_un_joven_paciente_en_una_sala_de_hospital__Foto_de_archivo1;
             this.ClientSize = new System.Drawing.Size(585, 562);
+            this.Controls.Add(this.BtnAtras);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEstadoCita);
+            this.Controls.Add(this.txtNumCita);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "CambiarEstadoCita";
@@ -66,6 +122,7 @@
             this.Load += new System.EventHandler(this.CambiarEstadoCita_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +130,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtNumCita;
+        private System.Windows.Forms.TextBox txtEstadoCita;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnAtras;
     }
 }
--- PrograFinalBD/PrograFinalBD/CambiarEstadoCita.cs
+++ PrograFinalBD/PrograFinalBD/CambiarEstadoCita.cs
@@ -13,6 +13,8 @@ namespace PrograFinalBD
     public partial class CambiarEstadoCita : Form
     {
         conexionBD nuevaconexion = new conexionBD();
+        string ventanaDeRetorno;
+        public string cedula_paciente = "";
         public CambiarEstadoCita()
         {
             InitializeComponent();
@@ -20,14 +22,45 @@ namespace PrograFinalBD
 
         private void CambiarEstadoCita_Load(object sender, EventArgs e)
         {
-            string query = "select cita.numeroCita, estadoCita, fechaCita, horaCita, nombreArea from cita, pacienteSolicitaCita ";
-            query = query + " where cita.numeroCita = pacienteSolicitaCita.numeroCita and pacienteSolicitaCita.cedula = 504100497";
-            nuevaconexion.seleccionarconjoin(dataGridView1, "cita,pacienteSolicitaCita", "numeroCita", query);
+            if (ventanaDeRetorno == "Pac")
+            {
+                string query = "select cita.numeroCita, estadoCita, fechaCita, horaCita, nombreArea from cita, pacienteSolicitaCita ";
+                query = query + " where cita.numeroCita = pacienteSolicitaCita.numeroCita and pacienteSolicitaCita.cedula = " + cedula_paciente;
+                nuevaconexion.seleccionarconjoin(dataGridView1, "cita,pacienteSolicitaCita", "numeroCita", query);
+            }
+            else
+            {
+                nuevaconexion.seleccionarValoresBaseDatosTodasLasTablas(dataGridView1, "cita", "numeroCita", "");
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string query = "update cita set estadoCita = '" + txtEstadoCita.Text + "' ";
+            nuevaconexion.actualizarValoresBaseDatos(query, "numeroCita", txtNumCita.Text);
+        }
 
+        private void BtnAtras_Click(object sender, EventArgs e)
+        {
+            if (ventanaDeRetorno == "Doc")
+            {
+                MenuDoctor nuevo = new PrograFinalBD.MenuDoctor();
+                nuevo.Show();
+                Hide();
+            }
+            else if (ventanaDeRetorno == "Enf")
+            {
+                MenuEnfermero nuevo = new PrograFinalBD.MenuEnfermero();
+                nuevo.Show();
+                Hide();
+            }
+            else if (ventanaDeRetorno == "Pac")
+            {
+                MenuPaciente nuevo = new PrograFinalBD.MenuPaciente();
+                nuevo.cedula = cedula_paciente;
+                nuevo.Show();
+                Hide();
+            }
         }
     }
 }
